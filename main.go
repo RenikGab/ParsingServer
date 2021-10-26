c3eda833dbccd30990a27351e14647ac58f9fccc
RenikGab <renik@inbox.ru>
2021-10-26 22:25:19 +0300

    +linux

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ type record struct {
 var Log *log.Logger
 
 func getLastDate() time.Time {
-	content, err := ioutil.ReadFile("download/coronaparsing/docs/data/12/data.json")
+	content, err := ioutil.ReadFile("./download/coronaparsing/docs/data/12/data.json")
+
 	if err != nil {
 		log.Fatal(err)
 	}
